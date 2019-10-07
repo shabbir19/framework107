040db68ec32bd549722cc415c96e3a767970d191
shabbir19 <55030692+shabbir19@users.noreply.github.com>
2019-10-07 12:43:48 -0400

    firt push

---
+++ src/base/Condition.java
@@ -0,0 +1,14 @@
+package base;
+
+import org.testng.annotations.Test;
+
+public class Condition {
+	@Test
+	public void  test(){
+	String a="this is a simple project";
+	int b=10;
+	System.out.println(a+b);
+	
+}
+}
+
